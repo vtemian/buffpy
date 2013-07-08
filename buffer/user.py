62aeb9d02f4745767e8aa832b70fc910f5159a7e
vtemian <vladtemian@gmail.com>
2013-07-08 18:14:50 +0300

    Using responseObject, send and parse a simple request for user resouce

--- buffer/user.py
+++ buffer/user.py
@@ -1,2 +1,9 @@
-class User(object):
-  pass
+from response import ResponseObject
+
+class User(ResponseObject):
+  REQUEST_URL = 'user.json'
+
+  def __init__(self, api):
+    response = api.get(self.REQUEST_URL)
+
+    super(User, self).__init__(response)
