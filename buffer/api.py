24b590d96b84457e848a0d22b00eca299cc53681
vtemian <vladtemian@gmail.com>
2013-07-08 15:11:37 +0300

    Buffer api session inherit from OAuth2Session, base class in rauth

--- buffer/api.py
+++ buffer/api.py
@@ -1,22 +1,15 @@
 from rauth import OAuth2Session
 
-BASE_URL = 'https://api.bufferapp.com/'
+BASE_URL = 'https://api.bufferapp.com/1/%s'
 
-class API(object):
+class API(OAuth2Session):
   '''
     Small and clean class that embrace all basic 
     operations with the buffer app
   '''
 
-  def __init__(self, client_id, client_secret, access_token=None):
-    self.session = OAuth2Session(client_id, client_secret, access_token)
+  def get(self, url):
+    if not self.access_token:
+      raise ValueError('Please set an access token first!')
 
-    self.call_url = None
-    if access_token:
-      self.set_access_token(access_token)
-
-  def set_access_token(self, access_token=None):
-    if not access_token or not isinstance(access_token, str):
-      raise ValueError('The access token is missing or is invalid')
-
-    self.call_url = BASE_URL + "%saccess_token=" + access_token
+    return super(OAuth2Session, self).get(url=BASE_URL % url)
