70159fb140ef17cea941b6bcfbb0decc40a2ec77
vtemian <vladtemian@gmail.com>
2013-07-08 15:10:49 +0300

    Added some examples

---
+++ examples/me.py
@@ -0,0 +1,10 @@
+from buffer.api import API
+
+from buffer.user import User
+
+token = '1/714ebdb617705ef9491a81fb21c1da42'
+api = API(client_id='51cc6dd5f882a8ba18000055', client_secret='83b019d154cae4d2c734d813b33e5e53')
+
+r = api.get('user.json?')
+print r.content
+#user = User(api=api)
