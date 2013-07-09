8ad6744aeb95ac6eb4ce003388e430f9cba6b36e
vtemian <vladtemian@gmail.com>
2013-07-09 11:29:50 +0300

    Simple example for profiles retrieving

---
+++ examples/profiles.py
@@ -0,0 +1,24 @@
+from pprint import pprint as pp
+
+from colorama import Fore
+
+from buffer.managers.profiles import Profiles
+from buffer.api import API
+
+# check http://bufferapp.com/developers/apps to retrieve a token
+# or generate one with the example
+token = '1/714ebdb617705ef9491a81fb21c1da42'
+
+# instantiate the api object
+api = API(client_id='51cc6dd5f882a8ba18000055', 
+          client_secret='83b019d154cae4d2c734d813b33e5e53', 
+          access_token=token)
+
+# get all profiles 
+profiles = Profiles(api=api).all()
+print profiles
+
+#profile = Profile(id='asd')
+
+#print profile.schedules
+#profile.schedules = 'bla bla'
