bc0d9e2b4403107f720d2b6a762f864b70ecea43
vtemian <vladtemian@gmail.com>
2013-07-09 17:19:40 +0300

    Added tests for schedulers updating

--- buffer/tests/test_profile.py
+++ buffer/tests/test_profile.py
@@ -23,3 +23,20 @@ def test_profile_schedules_getter():
 
   eq_(profile.schedules, '123')
   mocked_api.get.assert_called_once_with(url = PATHS['GET_SCHEDULES'] % 1, parser=json.loads)
+
+def test_profile_schedules_setter():
+  '''
+    Test schedules setter from buffer api
+  '''
+
+  mocked_api = MagicMock()
+  mocked_api.get.return_value = '123'
+
+  profile = Profile(mocked_api, mocked_response)
+
+  profile.schedules = {
+      'times': ['mo']
+  }
+
+  mocked_api.post.assert_called_once_with(url=PATHS['UPDATE_SCHEDULES'] % 1,
+      parser=json.loads, data='schedules[0][times][]=mo&')
