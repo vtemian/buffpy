d1a5402bfa8779e3857ce4330b1a9e7b0baec153
vtemian <vladtemian@gmail.com>
2013-07-10 11:01:16 +0300

    Test response oject for dict into dict into dict

---
+++ buffer/tests/test_response.py
@@ -0,0 +1,24 @@
+from nose.tools import eq_
+
+from buffer.response import ResponseObject
+
+def test_reponse_check_for_inception():
+  '''
+    Given a dict with a dict, it should convert all the dicts to ResponseObject
+  '''
+
+  awesome_dict = {
+    'key': 'value',
+    'second_dict': {
+      'key2': 'value2'
+    }
+  }
+
+  response = ResponseObject(awesome_dict)
+  response.key3 = 'value3'
+
+  eq_(response.key, 'value')
+  eq_(response.key3, 'value3')
+  eq_(response.second_dict, {'key2': 'value2'})
+  eq_(response.second_dict.key2, 'value2')
+
