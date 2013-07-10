624c133ba1afdb904e31742ac5f00a76859ab5b7
vtemian <vladtemian@gmail.com>
2013-07-10 11:00:46 +0300

    Write some docs for the response object

--- buffer/response.py
+++ buffer/response.py
@@ -1,4 +1,16 @@
 class ResponseObject(dict):
+  '''
+    Simple data structure that convert any dict to an empty object
+    where all the atributes are the keys of the dict, but also preserve a dict
+    behavior
+    e.g:
+
+      obj = ResponseObject({'a':'b'})
+      obj.key = 'value'
+
+      obj.a   => 'b'
+      obj     => {'a': 'b', 'key': 'value'}
+  '''
 
   def __init__(self, *args, **kwargs):
     super(ResponseObject, self).__init__(*args, **kwargs)
@@ -6,6 +18,9 @@ class ResponseObject(dict):
     self.__dict__ = self._check_for_inception(self)
 
   def _check_for_inception(self, root_dict):
+    '''
+      Used to check if there is a dict in a dict
+    '''
 
     for key in root_dict:
       if type(root_dict[key]) == dict:
