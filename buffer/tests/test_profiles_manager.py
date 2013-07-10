1f2deb95ba543bf05dd78f1df2e9ee6d17a2c4c3
vtemian <vladtemian@gmail.com>
2013-07-10 10:47:08 +0300

    Test profiles manager filterting method

--- buffer/tests/test_profiles_manager.py
+++ buffer/tests/test_profiles_manager.py
@@ -4,7 +4,7 @@ from nose.tools import eq_, raises
 from mock import MagicMock, patch
 
 from buffer.managers.profiles import Profiles
-from buffer.models.profile import PATHS
+from buffer.models.profile import Profile, PATHS
 
 mocked_response = {
   'name': 'me',
@@ -28,3 +28,27 @@ def test_profiles_manager_all_method():
     eq_(profiles, [1])
     mocked_api.get.assert_called_once_with(url=PATHS['GET_PROFILES'])
     mocked_profile.assert_called_once_with(mocked_api, {'a': 'b'})
+
+def test_profiles_manager_filter_method():
+  '''
+    Test basic profiles filtering based on some minimal criteria
+  '''
+
+  mocked_api = MagicMock()
+
+  profiles = Profiles(mocked_api, [{'a':'b'}, {'a': 'c'}])
+
+  eq_(profiles.filter(a='b'), [{'a': 'b'}])
+
+def test_profiles_manager_filter_method_empty():
+  '''
+    Test basic profiles filtering when the manager is empty
+  '''
+
+  mocked_api = MagicMock()
+  mocked_api.get.return_value = [{'a':'b'}, {'a': 'c'}]
+
+
+  profiles = Profiles(api=mocked_api)
+
+  eq_(profiles.filter(a='b'), [Profile(mocked_api, {'a': 'b'})])
