c5355e241244683ebf8275dc053c45f1f3ff8f29
vtemian <vladtemian@gmail.com>
2013-07-10 16:19:02 +0300

    Reorder the pendig updates

--- buffer/managers/updates.py
+++ buffer/managers/updates.py
@@ -4,6 +4,7 @@ PATHS = {
   'GET_PENDING': 'profiles/%s/updates/pending.json',
   'GET_SENT': 'profiles/%s/updates/sent.json',
   'SHUFFLE': 'profiles/%s/updates/shuffle.json',
+  'REORDER': 'profiles/%s/updates/reorder.json',
 }
 
 class Updates(list):
@@ -64,5 +65,27 @@ class Updates(list):
       post_data += 'count=%s&' % count
     if utc:
       post_data += 'utc=%s' % utc
+
+    return self.api.post(url=url, data=post_data)
+
+  def reorder(self, updates_ids, offset=None, utc=None):
+    '''
+      Edit the order at which statuses for the specified social media profile will
+      be sent out of the buffer.
+    '''
+
+    url = PATHS['REORDER'] % self.profile_id
+
+    order_format = "order[]=%s&"
+    post_data = ''
+
+    if offset:
+      post_data += 'offset=%s&' % offset
+
+    if utc:
+      post_data += 'utc=%s&' % utc
+
+    for update in updates_ids:
+      post_data += order_format % update
 
     return self.api.post(url=url, data=post_data)
