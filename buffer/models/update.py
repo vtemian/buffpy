bad077f312b5a1446fc6d88683d3a4cf22656eb6
vtemian <vladtemian@gmail.com>
2013-07-10 15:36:30 +0300

    Get updates interactions

--- buffer/models/update.py
+++ buffer/models/update.py
@@ -2,14 +2,30 @@ from buffer.response import ResponseObject
 
 PATHS = {
   'GET_UPDATE': 'updates/%s.json',
+  'GET_INTERACTIONS': 'updates/%s/interactions.json',
 }
 
 class Update(ResponseObject):
 
   def __init__(self, api, id=None, raw_response=None):
-    self.api = api
 
     if id and not raw_response:
-      raw_response = self.api.get(url=PATHS['GET_UPDATE'] % id)
+      raw_response = api.get(url=PATHS['GET_UPDATE'] % id)
 
     super(Update, self).__init__(raw_response)
+
+    self.api = api
+    self.__interactions = []
+
+  @property
+  def interactions(self):
+    interactions = []
+    url = PATHS['GET_INTERACTIONS'] % self.id
+
+    response = self.api.get(url=url)
+    for interaction in response['interactions']:
+      interactions.append(RawObject(interaction))
+
+    self.__interactions = interactions
+
+    return self.__interactions
