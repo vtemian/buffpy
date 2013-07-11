973c6a541deadb5a4b7c23dae191acf9d4c1be27
vtemian <vladtemian@gmail.com>
2013-07-11 16:55:29 +0300

    Test link's shares retrieving using get_share method

--- buffer/tests/test_link.py
+++ buffer/tests/test_link.py
@@ -14,3 +14,19 @@ def test_links_shares():
   link = Link(api=mocked_api, url='www.google.com')
 
   eq_(link, {'shares': 123, 'url': 'www.google.com', 'api': mocked_api})
+  mocked_api.get.assert_called_once_with(url='links/shares.json?url=www.google.com')
+
+def test_links_get_shares():
+  '''
+    Test link's shares retrieving method
+  '''
+
+  mocked_api = MagicMock()
+  mocked_api.get.return_value = {'shares': 123}
+
+  link = Link(api=mocked_api, url='www.google.com')
+
+  eq_(link, {'shares': 123, 'url': 'www.google.com', 'api': mocked_api})
+  eq_(link.get_shares(), 123)
+  mocked_api.get.assert_any_call(url='links/shares.json?url=www.google.com')
+  eq_(mocked_api.get.call_count, 2)
