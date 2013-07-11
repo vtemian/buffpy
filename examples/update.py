e7c5c112af2c6314f8ecddc60c4de27475d3864d
vtemian <vladtemian@gmail.com>
2013-07-11 13:57:19 +0300

    Split updates related examples in update and profile&updates

---
+++ examples/update.py
@@ -0,0 +1,36 @@
+from pprint import pprint as pp
+
+from colorama import Fore
+
+from buffer.models.update import Update
+from buffer.managers.profiles import Profiles
+from buffer.managers.updates import Updates
+from buffer.api import API
+
+# check http://bufferapp.com/developers/apps to retrieve a token
+# or generate one with the example
+token = '1/714ebdb617705ef9491a81fb21c1da42'
+
+# instantiate the api object
+api = API(client_id='51cc6dd5f882a8ba18000055',
+          client_secret='83b019d154cae4d2c734d813b33e5e53',
+          access_token=token)
+
+# retrieve a single update based on an id
+update = Update(api=api, id='51de8d33e48c051712000019')
+print update
+
+# get update's interactions
+print update.interactions
+
+# edit
+update = update.edit(text="Hey!")
+
+# publish now
+update.publish()
+
+# move to top
+update.move_to_top
+
+# delete
+update.delete()
---
+++ examples/updates_and_profiles.py
@@ -0,0 +1,36 @@
+from pprint import pprint as pp
+
+from colorama import Fore
+
+from buffer.managers.profiles import Profiles
+from buffer.managers.updates import Updates
+
+from buffer.api import API
+
+# check http://bufferapp.com/developers/apps to retrieve a token
+# or generate one with the example
+token = '1/714ebdb617705ef9491a81fb21c1da42'
+
+# instantiate the api object
+api = API(client_id='51cc6dd5f882a8ba18000055',
+          client_secret='83b019d154cae4d2c734d813b33e5e53',
+          access_token=token)
+
+# get all pending updates of a social network profile
+profile = Profiles(api=api).filter(service='twitter')[0]
+print profile.updates.pending
+
+# get all sent updates of a social network profile
+print profile.updates.sent
+
+# retrieve all update's interactions
+print profile.updates.sent[0].interactions
+
+# shuffle updates
+print profile.updates.shuffle(count=10)
+
+# reorder updates
+print profile.updates.reorder(['51dd27629f7fdf520d00009a'])
+
+# create an update
+print profile.updates.new("Testing", now=True)
