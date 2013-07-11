775baff2a8cc3b0a396b5b8cf6224688b965d5c3
vtemian <vladtemian@gmail.com>
2013-07-11 11:39:43 +0300

    Test sent updates retrieving

--- buffer/tests/test_updates_manager.py
+++ buffer/tests/test_updates_manager.py
@@ -8,7 +8,7 @@ from buffer.models.update import Update
 
 def test_updates_manager_pending():
   '''
-    Test basic pending updates retreiving
+    Test basic pending updates retrieving
   '''
 
   mocked_api = MagicMock()
@@ -22,3 +22,21 @@ def test_updates_manager_pending():
 
   mocked_api.get.assert_called_once_with(url='profiles/1/updates/pending.json')
   eq_(pendings, [Update(api=mocked_api, raw_response={'text': 'hey'})])
+
+def test_udpates_manager_sent():
+  '''
+    Test basic sent updates retrieving
+  '''
+
+  mocked_api = MagicMock()
+  mocked_api.get.return_value = {
+    'updates': [{
+      'text': 'sent'
+    }]
+  }
+
+  sent = Updates(api=mocked_api, profile_id=1).sent
+  assert_update = Update(api=mocked_api, raw_response={'text': 'sent'})
+
+  mocked_api.get.assert_called_once_with(url='profiles/1/updates/sent.json')
+  eq_(sent, [assert_update])
