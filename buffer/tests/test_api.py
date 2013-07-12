b71eebf2c367288da9a274a49e1262fd709c8c31
vtemian <vladtemian@gmail.com>
2013-07-12 14:14:50 +0300

    Test api get request

--- buffer/tests/test_api.py
+++ buffer/tests/test_api.py
@@ -5,18 +5,19 @@ from mock import MagicMock, patch
 
 from buffer.api import API
 
-def test_api_info_request():
+def test_api_get_request():
   '''
-    Test simply api info request
+    Test simply api get request
   '''
 
-  with patch('buffer.api.OAuth2Session') as mocked_session:
+  with patch('buffer.api.OAuth2Session') as mocked_oauth2:
+    mocked_session = MagicMock()
 
     mocked_response = MagicMock()
     mocked_response.content = json.dumps({'status': 'ok'})
     mocked_session.get.return_value = mocked_response
 
-    mocked_session.access_token = 'access_token'
+    mocked_oauth2.return_value = mocked_session
 
     api = API(client_id='1', client_secret='2', access_token='access_token')
 
