55ed4721eef8d56cd6e63f66c7ade24693c61d5f
vtemian <vladtemian@gmail.com>
2013-07-12 15:07:10 +0300

    Added doc for user model

--- buffer/models/user.py
+++ buffer/models/user.py
@@ -1,6 +1,10 @@
 from buffer.response import ResponseObject
 
 class User(ResponseObject):
+  '''
+    A user represents a single Buffer user account.
+  '''
+
   REQUEST_URL = 'user.json'
 
   def __init__(self, api):
