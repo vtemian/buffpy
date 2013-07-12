a2427f4cda216332bc98dc070e7bf12ebb6af526
vtemian <vladtemian@gmail.com>
2013-07-12 14:18:58 +0300

    Test post api request

--- buffer/tests/test_api.py
+++ buffer/tests/test_api.py
@@ -20,5 +20,27 @@ def test_api_get_request():
     mocked_oauth2.return_value = mocked_session
 
     api = API(client_id='1', client_secret='2', access_token='access_token')
-
     api.get(url="hey")
+
+    mocked_session.get.assert_called_once_with(url='https://api.bufferapp.com/1/hey')
+
+def test_api_post_request():
+  '''
+    Test simply api post request
+  '''
+
+  with patch('buffer.api.OAuth2Session') as mocked_oauth2:
+    mocked_session = MagicMock()
+
+    mocked_response = MagicMock()
+    mocked_response.content = json.dumps({'status': 'ok'})
+    mocked_session.post.return_value = mocked_response
+
+    mocked_oauth2.return_value = mocked_session
+
+    api = API(client_id='1', client_secret='2', access_token='access_token')
+    api.post(url='hey', data='new=True')
+
+    headers = {'Content-Type':'application/x-www-form-urlencoded'}
+    mocked_session.post.assert_called_once_with(
+        url='https://api.bufferapp.com/1/hey', headers=headers, data='new=True')
