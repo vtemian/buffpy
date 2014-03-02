4df997fbdae84b866a823e2253653229e956fa81
vtemian <vladtemian@gmail.com>
2014-03-02 23:06:43 +0200

    Upgrade some dependencies

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 from setuptools import setup
 
 requires_list = [
-  'argparse>=1.2.1',
-  'colorama>=0.2.5',
-  'coverage>=3.6',
-  'mock>=1.0.1',
-  'nose>=1.3.0',
-  'rauth>=0.5.5',
-  'requests>=1.2.3',
-  'wsgiref>=0.1.2']
+    'colorama==0.2.7',
+    'coverage==3.7.1',
+    'mock==1.0.1',
+    'nose==1.3.0',
+    'rauth==0.6.2',
+    'requests==1.2.3',
+]
 
 setup(name='buffpy',
-      version='1.05b',
+      version='1.06',
       platforms='any',
       description='Python library for Buffer App',
       author='Vlad Temian',
@@ -21,6 +20,6 @@ setup(name='buffpy',
       include_package_data=True,
       install_requires=requires_list,
       classifiers=[
-        'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 2.7',
       ]
 )
