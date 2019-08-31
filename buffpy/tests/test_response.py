ef466384589a625e499b84ea97d3357dcbffb52b
Vlad Temian <vladtemian@gmail.com>
2019-08-31 20:05:57 +0300

    Migrate response test to pytest

--- buffpy/tests/test_response.py
+++ buffpy/tests/test_response.py
@@ -1,23 +1,22 @@
-from nose.tools import eq_
-
 from buffpy.response import ResponseObject
 
+
 def test_reponse_check_for_inception():
-  '''
+    """
     Given a dict with a dict, it should convert all the dicts to ResponseObject
-  '''
+    """
 
-  awesome_dict = {
-    'key': 'value',
-    'second_dict': {
-      'key2': 'value2'
+    awesome_dict = {
+        "key": "value",
+        "second_dict": {
+            "key2": "value2"
+        }
     }
-  }
 
-  response = ResponseObject(awesome_dict)
-  response.key3 = 'value3'
+    response = ResponseObject(awesome_dict)
+    response.key3 = "value3"
 
-  eq_(response.key, 'value')
-  eq_(response.key3, 'value3')
-  eq_(response.second_dict, {'key2': 'value2'})
-  eq_(response.second_dict.key2, 'value2')
+    assert response.key == "value"
+    assert response.key3 == "value3"
+    assert response.second_dict["key2"] == "value2"
+    assert response.second_dict.key2 == "value2"
