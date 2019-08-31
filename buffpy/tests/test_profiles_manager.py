53f2e3e5b58b001743bdedb479697150a9205b3f
Vlad Temian <vladtemian@gmail.com>
2019-08-31 20:12:23 +0300

    Migrate profiles manager tests to pytest

--- buffpy/tests/test_profiles_manager.py
+++ buffpy/tests/test_profiles_manager.py
@@ -1,52 +1,44 @@
-from nose.tools import eq_
-from mock import MagicMock, patch
+from unittest.mock import MagicMock, patch
 
 from buffpy.managers.profiles import Profiles
 from buffpy.models.profile import Profile, PATHS
 
-mocked_response = {
-  'name': 'me',
-  'service': 'twiter',
-  'id': 1
+
+MOCKED_RESPONSE = {
+    "name": "me",
+    "service": "twiter",
+    "id": 1
 }
 
+
 def test_profiles_manager_all_method():
-  '''
-    Test basic profiles retrieving
-  '''
+    """ Should retrieve profile info. """
 
-  mocked_api = MagicMock()
-  mocked_api.get.return_value = [{'a':'b'}]
+    mocked_api = MagicMock()
+    mocked_api.get.return_value = [{"a": "b"}]
 
-  with patch('buffpy.managers.profiles.Profile') as mocked_profile:
-    mocked_profile.return_value = 1
+    with patch("buffpy.managers.profiles.Profile", return_value=1) as mocked_profile:
+        profiles = Profiles(api=mocked_api).all()
 
-    profiles = Profiles(api=mocked_api).all()
+        assert profiles == [1]
+        mocked_api.get.assert_called_once_with(url=PATHS["GET_PROFILES"])
+        mocked_profile.assert_called_once_with(mocked_api, {"a": "b"})
 
-    eq_(profiles, [1])
-    mocked_api.get.assert_called_once_with(url=PATHS['GET_PROFILES'])
-    mocked_profile.assert_called_once_with(mocked_api, {'a': 'b'})
 
 def test_profiles_manager_filter_method():
-  '''
-    Test basic profiles filtering based on some minimal criteria
-  '''
+    """ Should filter based on criteria. """
 
-  mocked_api = MagicMock()
+    mocked_api = MagicMock()
+    profiles = Profiles(mocked_api, [{"a": "b"}, {"a": "c"}])
+    assert profiles.filter(a="b") == [{"a": "b"}]
 
-  profiles = Profiles(mocked_api, [{'a':'b'}, {'a': 'c'}])
-
-  eq_(profiles.filter(a='b'), [{'a': 'b'}])
 
 def test_profiles_manager_filter_method_empty():
-  '''
-    Test basic profiles filtering when the manager is empty
-  '''
+    """ Should filter if profile manager is None. """
 
-  mocked_api = MagicMock()
-  mocked_api.get.return_value = [{'a':'b'}, {'a': 'c'}]
+    mocked_api = MagicMock()
+    mocked_api.get.return_value = [{"a": "b"}, {"a": "c"}]
 
+    profiles = Profiles(api=mocked_api)
 
-  profiles = Profiles(api=mocked_api)
-
-  eq_(profiles.filter(a='b'), [Profile(mocked_api, {'a': 'b'})])
+    assert profiles.filter(a="b") == [Profile(mocked_api, {"a": "b"})]
